91b940b2bb1198d6fce0fbb95ef65d55ec1aba93
garry <garry4978@gmail.com>
2023-01-16 17:32:19 +0300

    Функциональные интерфейсы и лямбда-выражения

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,59 @@
+import java.util.function.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+        Predicate<Integer> pos = new Predicate<Integer>() {
+           @Override
+            public boolean test(Integer integer) {
+                if(integer> 0) return true;
+                return false;
+            }
+        };
+
+
+        Predicate<Integer> posi = integer -> {
+            if(integer> 0) return true;
+            return false;
+        };
+
+        System.out.println(pos.test(11) + " " + posi.test( -5));
+
+
+        // Задание 2
+
+        Consumer<String> name = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println( "Привет" + s);
+            }
+        };
+
+        Consumer<String> nam = s -> System.out.println( "Привет" + s);
+
+       nam.accept("Петя"); name.accept("Вася");
+
+        //  Задание 3
+
+        Function<Double,Long> dToL = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double aDouble) {
+                return  Math.round(aDouble);
+            }
+        };
+
+        Function<Double,Long> dTooL = aDouble -> Math.round(aDouble);
+
+        System.out.println(dToL.apply(12.12) + " " + dTooL.apply(33.22));
+    }
+
+    //  Задание 4
+
+    Supplier<Integer> rand = new Supplier<Integer>() {
+        @Override
+        public Integer get() {
+            return (Math.random()*100) ;
+        }
     }
 }
